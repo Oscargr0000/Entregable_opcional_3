c140eebbe5daf43ada47fea260bff351e0de0a63
Oscargr0000 <oscargr0000@gmail.com>
2022-02-18 23:20:06 +0100

    :)

---
+++ Assets/Scripts/EntregableOpcional3.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntregableOpcional3 : MonoBehaviour
+{
+
+    public int divisor = 3;
+
+    public int[] intArray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ejercicio 1
+
+        /*for (int i = 1; i < 101; i++)
+        {
+            Debug.Log(i);
+        }
+        */
+
+        //Ejercicio 2
+
+        /*for (int i = 1; i < 21; i++)
+        {
+            Debug.Log($"Hola, holita, vecinito");
+        }
+        */
+
+        //Ejercicio 3
+
+        /*for (int i=1; i * divisor < 500; i++)
+        {
+            Debug.Log(i * divisor);
+        }
+        */
+
+        //Ejercicio 4
+
+
+        /*
+        for(int i= 0; i < 5; i++)
+        {
+            Debug.Log(intArray[i]);
+        }
+        */
+
+
+        // NO ME HA DADO TIEMPO A MAS :)
+
+    }   
+
+ 
+    void Update()
+    {
+        
+    }
+}
